5ce2d021e0f5026370708bbe540e008f56c55b3f
Mihail Nazarov <msnazarow@gmail.com>
2021-09-18 15:10:35 +0300

    Initial Commit

---
+++ SwiftPiscine.Day02/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftPiscine.Day02
+//
+//  Created by out-nazarov2-ms on 18.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
